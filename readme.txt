d9fd5c42c322b193152ef9cc9acaed27ffe9716e
davidwalsh83 <davidwalsh83@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-21 01:40:45 +0000

    Moving to 1.2
    git-svn-id: https://plugins.svn.wordpress.org/remove-posts-in-category-from-homepage/tags/1.02@601835 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,49 @@
+=== Plugin Name ===
+Contributors: davidwalsh83
+Tags: categories, posts
+Requires at least: 3.0.1
+Tested up to: 3.4.2
+Stable tag: 1.02
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin allows WordPres admins to prevent posts in a given category from dislaying on the homepage / main loop.
+
+== Description ==
+
+This plugin allows WordPres admins to prevent posts in a given category from dislaying on the homepage / main loop.
+
+A new checkbox will display on the "Add New Category" and "Edit Category" screens -- it's that simple!
+
+== Installation ==
+
+Installation is easy:
+
+1. Upload `remove-category-from-loop.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= Will posts in given categories still display within the RSS feed? =
+
+Yes, posts in marked categories will still display in the RSS feed in their normal position.
+
+= What is this plugin's option key? =
+
+The option key is `remove-loop-cats`;  all categories marked not to display in the main loop are stored in this option to keep the DB footprint as low as possible.
+
+== Screenshots ==
+
+1. The checkbox appears on the "Add New Category" and "Edit Category" forms.
+
+== Changelog ==
+
+= 1.02 =
+* Additional logical check at time of install to ensure an array is present
+
+= 1.01 =
+* Improved layout of both the "Add Category" and "Edit Category" screens
+* Fixed option key value within the FAQ
+
+= 1.0 =
+* Initial release of plugin
---
+++ remove-category-from-loop.php
@@ -0,0 +1,99 @@
+<?php
+/*
+Plugin Name: Remove Posts in Category From Homepage
+Plugin URI: http://davidwalsh.name/category-loop
+Description: Allows the blogger to prevent posts within a given category from displaying in the main loop
+Author: David Walsh
+Version: 1.02
+Author URI: http://davidwalsh.name
+*/
+?><?php 
+
+	$RCFH_LOOP_LABEL = 'Remove from main loop';
+	$RCFH_LOOP_DESCRIPTION = 'Check this box if you would like posts in this category to be prevented from displaying within the main loop.';
+	$RCFH_LOOP_OPTION_KEY = 'remove-loop-cats';
+
+	// Add the extra field to the EDIT category page
+	add_action('category_edit_form_fields', 'rcfh_loop_field_edit');
+	function rcfh_loop_field_edit($term) {
+		global $RCFH_LOOP_LABEL, $RCFH_LOOP_DESCRIPTION, $RCFH_LOOP_OPTION_KEY;
+
+		$value = get_option($RCFH_LOOP_OPTION_KEY);
+		if(!$value) {
+			$value = array();
+		}
+
+		$checked = in_array($term->term_id, $value);
+ ?>
+	<tr class="form-field">
+		<th scope="row" valign="top"><label for="removeMainLoop"><?php _e($RCFH_LOOP_LABEL); ?></label></th>
+		<td>
+			<label for="removeMainLoop">
+				&nbsp;&nbsp;<input type="checkbox" name="remove-loop" id="removeMainLoop"<?php echo $checked ? ' checked="checked"' : ''; ?> value="1" style="width:auto;" />
+				<span class="description"><?php _e($RCFH_LOOP_DESCRIPTION); ?></span>
+			</label>
+		</td>
+	</tr>
+<?php } 
+
+	// Add the extra field to the ADD category page
+	add_action('category_add_form_fields', 'rcfh_loop_field_create');
+	function rcfh_loop_field_create() { 
+		global $RCFH_LOOP_LABEL, $RCFH_LOOP_DESCRIPTION;
+?>
+	<div class="form-field">
+		<label for="removeMainLoop">
+			<?php _e($RCFH_LOOP_LABEL); ?><br />
+		<p>
+			<input type="checkbox" name="remove-loop" id="removeMainLoop" value="1" style="width:auto;" />
+			<span style="display: inline-block;"><?php _e($RCFH_LOOP_DESCRIPTION); ?></span>
+		</p>
+		</label>
+	</div>
+<?php }
+
+	// Add action for saving extra category information
+	add_action('edit_category', 'rcfh_save_loop_value');
+	add_action('create_category', 'rcfh_save_loop_value');
+	function rcfh_save_loop_value($id) {
+		global $RCFH_LOOP_OPTION_KEY;
+
+		$value = get_option($RCFH_LOOP_OPTION_KEY);
+		if(!$value) {
+			$value = array();
+		}
+
+		// Add or remove the value
+		if(isset($_POST['remove-loop'])) {
+			array_push($value, $id);
+		}
+		else {
+			$value = array_diff($value, array($id));
+		}
+
+		// Ensure no duplicates, just for cleanliness
+		$value = array_unique(array_values($value));
+
+		// Save
+		update_option($RCFH_LOOP_OPTION_KEY, $value);
+	}
+
+	// Filter for removing said category posts from main loop
+	add_action('pre_get_posts', 'rcfh_prevent_posts');
+	function rcfh_prevent_posts($query) {
+		global $RCFH_LOOP_OPTION_KEY;
+
+		// Only remove categories if it's the main query/homepage
+		if($query->is_home() && $query->is_main_query()) {
+			$value = get_option($RCFH_LOOP_OPTION_KEY);
+			if(!is_array($value)) {
+				$value = array();
+			}
+
+			// Modify query to remove posts which shouldn't be shown
+			if(count($value)) {
+				$query->set('cat', '-'.implode(',-', $value));
+			}
+		}
+	}
+?>
